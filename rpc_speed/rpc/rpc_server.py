59b479f947d781e279c031a8234f95bc3b42a800
Aleksey Sergievskiy <vivers83@yandex.ru>
2024-04-21 22:47:19 +0300

    update

--- rpc_speed/rpc/rpc_server.py
+++ rpc_speed/rpc/rpc_server.py
@@ -1,4 +1,3 @@
-import asyncio
 import json
 import logging
 
@@ -22,42 +21,34 @@ class RPCServer:
     queue: AbstractQueue
 
     def __init__(
-        self, action, logger: logging.Logger = logging.getLogger(__name__)
+            self, action, logger: logging.Logger = logging.getLogger(__name__)
     ) -> None:
         self.settings = RabbitMQSettings()
         self.queue_name = RPCSettings().rpc_queue_name
         self.action = action
         self.logger = logger
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(self._connect())
 
     async def start(self) -> None:
         self.logger.info(f"{self.__class__.__name__} start.")
-        try:
-            async with self.queue.iterator() as queue_iterator:
-                message: AbstractIncomingMessage
 
-                async for message in queue_iterator:
+        async with self.queue.iterator() as queue_iterator:
+            message: AbstractIncomingMessage
 
-                    async with message.process(requeue=False):
-                        try:
-                            assert message.reply_to is not None, f"Bad message {message}"
-                            response = await self._execute_action(message.body)
-                        except Exception as e:
-                            self.logger.exception("Processing error")
-                            response = Response(status="ERROR", message=str(e))
-                            await self._reply_to(message, response.to_bytes())
-                            continue
-                        await self._reply_to(message, str(response).encode("utf-8"))
+            async for message in queue_iterator:
 
-        except asyncio.CancelledError:
-            pass
-        finally:
-            await self.connection.close()
-        self.logger.info(f"{self.__class__.__name__} stop.")
+                async with message.process(requeue=False):
+                    try:
+                        assert message.reply_to is not None, f"Bad message {message}"
+                        response = await self._execute_action(message.body)
+                    except Exception as e:
+                        self.logger.exception("Processing error")
+                        response = Response(status="ERROR", message=str(e))
+                        await self._reply_to(message, response.to_bytes())
+                        continue
+                    await self._reply_to(message, str(response).encode("utf-8"))
 
     async def _reply_to(
-        self, message: AbstractIncomingMessage, response: bytes
+            self, message: AbstractIncomingMessage, response: bytes
     ) -> None:
         await self.exchange.publish(
             Message(
@@ -68,12 +59,16 @@ class RPCServer:
         )
         self.logger.debug(f" [x] Sent {response!r}")
 
-    async def _connect(self) -> "RPCServer":
+    async def connect(self):
         self.connection = await connect(self.settings.dsn(True))
         self.channel = await self.connection.channel()
         self.exchange = self.channel.default_exchange
         self.queue = await self.channel.declare_queue(self.queue_name)
-        return self
+        self.logger.info(f"{self.__class__.__name__} connected.")
+
+    async def disconnect(self):
+        await self.connection.close()
+        self.logger.info(f"{self.__class__.__name__} disconnected.")
 
     async def _execute_action(self, params: bytes) -> bytes:
         data = json.loads(params.decode("utf-8"))
